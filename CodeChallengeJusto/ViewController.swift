3c2b838a52c9cac8329632d2833310415293192f
Jesus Loaiza Herrera <loaizaherrerajesus@gmail.com>
2022-08-06 16:00:28 -0500

    Initial Commit

---
+++ CodeChallengeJusto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeChallengeJusto
+//
+//  Created by Jesus Loaiza Herrera on 06/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
